d1c0e2fbeff71f1817b33ae91c5e46e6ba0534b0
Chiozzi Emanuele <chiozzi.emanuele@osdbsesto>
2021-01-28 13:26:30 +0100

    add home, name, index get request

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var http = require('http');
+//var http = require('http');
 var express = require('express');
+var apiServer = express();
 
 var hostname = 'localhost';
 var port = 3000;
+var path = require('path');
 
-var server = http.createServer((req, res) => {
-    console.log("req:",req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Hello there");
+var name = "NodeJSEx1";
+
+apiServer.listen(port, hostname, () => {
+    console.log("Running at http://%s:%d/", hostname, port)
 });
 
-server.listen(port, hostname, () => {
-    console.log("Running at http://%s:%d/", hostname, port)
+apiServer.get("/name", (req, res) => {
+    console.log("Recieved \"name\" get request");
+    res.send("Server name: "+ name);
+});
+
+apiServer.get("/", (req, res) => {
+    res.send("Welcome!");
+});
+
+apiServer.get("/index", (req, res) => {
+    console.log("Recieved \"index\" get request");
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
